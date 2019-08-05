63e609201bdbe9555c4d6b82386bd4a126876850
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-08-05 10:50:42 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Comment extends Component {
   render(){
     return (
-      {this.component.Comment}
+      <div>
+      {this.props.commentText}
+      </div>
     )
   }
 }
