42ed4ece4fd95370d47a23c97a2015c33b243745
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-08-05 10:52:28 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Comment extends Component {
+export default class comment extends Component {
   render(){
     return (
       <div>
